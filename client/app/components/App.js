55e8bf7cee4f9283880398dbe8079b84100f8231
Raakesh Madana <madanaraakesh@gmail.com>
2018-06-29 16:33:50 -0500

    Remove unused import

--- client/app/components/App.js
+++ client/app/components/App.js
@@ -3,7 +3,6 @@ import { ConnectedRouter } from 'connected-react-router'
 import { Route } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import SignUpForm from './SignUpForm'
-import Form from './Form'
 
 const App = ({ history }) => (
   <ConnectedRouter history={history}>
