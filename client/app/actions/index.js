e969b162e2e63a6d4e8daa3aeb8c7bd6381d4fbf
Raakesh Madana <madanaraakesh@gmail.com>
2018-06-29 16:31:58 -0500

    Fix URL

--- client/app/actions/index.js
+++ client/app/actions/index.js
@@ -1,20 +1,7 @@
 import * as types from '../constants/actionTypes'
 
-/*export const signUp = (uname, pwd) => ({
-  type: types.SIGN_UP,
-  uname,
-  pwd
-})*/
-
-/* Use the fetch api to post the data to the server 
- * Return a function instead of state and use redux-thunk middleware to
- * execute that function
- * Dispatch either AUTH_SUCCESS/AUTH_FAILURE and redirect
- * Redirect using conditional rendering
- */
-
 export const signUp = (uname, pwd) => dispatch => {
-  return fetch('localhost:3000/register' , {
+  return fetch('http://localhost:3000/register' , {
     method: 'POST',
     body: JSON.stringify({ uname, pwd }),
     headers: new Headers({
@@ -24,6 +11,7 @@ export const signUp = (uname, pwd) => dispatch => {
   .then(
     response => {
       if(response.ok) {
+        console.log("sign up success");
         dispatch(authSuccess(uname));
       } else {
         dispatch(authFailure());
