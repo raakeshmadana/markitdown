dfff0e8f050bda87697dbd436bfcf1194d2d351c
Raakesh Madana <madanaraakesh@gmail.com>
2018-07-01 14:52:33 -0500

    Fix signup

--- server/routes/register.js
+++ server/routes/register.js
@@ -6,7 +6,7 @@ const Account = require('../models/account');
 router.post('/', function(req, res){
   Account.register(new Account({ username: req.body.username }), req.body.password, function(err, account){
     if(err){
-      res.json(0);
+      res.sendStatus(401);
     }
     passport.authenticate('local')(req, res, function() {
       res.json(req.body.uname);
